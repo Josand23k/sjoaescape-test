228d9471d4d6f720bcd8cab2c6185bd282f09e3e
Jose Sandoval <teotladventures@gmail.com>
2022-12-15 22:38:31 +0100

    dropdown in contact page and add css

--- script.js
+++ script.js
@@ -25,3 +25,13 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
 }
+
+// -- TOPNAV RESPONSIVE DROPDOWN MENU -- //
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
